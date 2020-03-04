52d61f31835e8e97c0afd39a0924e05c73441be1
Nicholas-Lin <Nicholas@d-172-27-98-81.dhcp.virginia.edu>
2020-03-04 09:46:32 -0500

    Fixed php comment

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-/**
- * FileName: index.php
- * Author: Nicholas Lin
- * Date: 3/3/20
- * Description: For Heroku functionality (required for buildpack detection)
- */
-
 <?php include_once("index.html"); ?>
