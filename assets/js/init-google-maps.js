67a5d23c2144c553e99bfafc831a4e1a616379c9
Nicholas-Lin <Nicholas@mlin-mac15.fios-router.home>
2020-05-16 22:46:17 -0400

    Fixed API issues

--- assets/js/init-google-maps.js
+++ assets/js/init-google-maps.js
@@ -11,5 +11,9 @@ const google_maps_api_key = config.GOOGLE_MAPS_API_KEY;
 script.src = 'https://maps.googleapis.com/maps/api/js?key='+ google_maps_api_key +'&callback=initMap';
 script.defer = true;
 script.async = true;
+// Attach your callback function to the `window` object
+window.initMap = function() {
+    // JS API is loaded and available
+  };
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
